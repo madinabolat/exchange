8f420d566447658f2350d063250f26d04ee96eee
madinabolat <madinabolat01@gmail.com>
2026-06-11 22:23:50 -0400

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,9 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        // Google "Coinbase WebSocket API BTC-USD"
+        // "Coinbase WebSocket API BTC-USD"
+        //polling vs web socket
+        //polling - discrete
+        //web socket - keeps flowing
     }
 }
