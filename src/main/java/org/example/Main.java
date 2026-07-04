507ae6ba2393cbff1b7bcb36095ed445b3b6b4fb
madinabolat <madinabolat01@gmail.com>
2026-07-03 22:05:38 -0400

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,11 +8,6 @@ import org.example.storage.OrderStorage;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        //https://docs.cdp.coinbase.com/coinbase-app/advanced-trade-apis/guides/websocket
-        // Market Data Endpoint: wss://advanced-trade-ws.coinbase.com
-        //https://www.coinbase.com/advanced-trade/spot/BTC-USD
-        //https://exchange.coinbase.com/trade/BTC-USD
-
         OrderStorage orderStorage = new OrderStorage();
         CoinbaseWebSocketClient coinbaseWebSocketClient = new CoinbaseWebSocketClient(orderStorage);
         coinbaseWebSocketClient.connect();
