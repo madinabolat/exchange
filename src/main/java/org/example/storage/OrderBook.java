c22b7b4a6abd03b9c67d57e637ee8e58cc0dae34
madinabolat <madinabolat01@gmail.com>
2026-07-14 21:33:40 -0400

    Update OrderBook.java

--- src/main/java/org/example/storage/OrderBook.java
+++ src/main/java/org/example/storage/OrderBook.java
@@ -7,5 +7,11 @@ import java.util.PriorityQueue;
 public class OrderBook {
     PriorityQueue<Order> buyBook;
     PriorityQueue<Order> sellBook;
-    
+
+    public OrderBook(){
+        this.buyBook = new PriorityQueue<>((Order order1, Order order2) -> (int) (order1.getPriceLevel() - order2.getPriceLevel()));
+        this.sellBook = new PriorityQueue<>((Order order1, Order order2) -> (int) (order1.getPriceLevel() - order2.getPriceLevel()));
+        //if order1 price < order 2 price -> negative -> order1 comes first, order 2 after. so this is sorted by smallest first.
+
+    }
 }
