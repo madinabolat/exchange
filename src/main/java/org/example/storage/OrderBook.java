2f76602d7e6c5df2c1c622a859c6762add4e249d
madinabolat <madinabolat01@gmail.com>
2026-07-14 21:19:37 -0400

    Create OrderBook.java

---
+++ src/main/java/org/example/storage/OrderBook.java
@@ -0,0 +1,11 @@
+package org.example.storage;
+
+import org.example.entity.Order;
+
+import java.util.PriorityQueue;
+
+public class OrderBook {
+    PriorityQueue<Order> buyBook;
+    PriorityQueue<Order> sellBook;
+    
+}
